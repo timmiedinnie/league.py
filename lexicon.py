59b065b87412dd55db8e55318553177a4cab07b4
timothyckl <timothychia.21@ichat.sp.edu.sg>
2023-06-17 00:02:10 +0800

    sepereated endpoints

--- lexicon.py
+++ lexicon.py
@@ -27,10 +27,38 @@ class Lexicon():
         'VN2',   # Vietnam
     ]
 
-    ENDPOINTS = {
-        'account': 'riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}',
+    _CHAMPION_MASTERY_V4 = {
         'champion_masteries': 'lol/champion-mastery/v4/champion-masteries/by-puuid/{puuid}',  # get all champion mastery entries sorted by number of champion points descending
         'champion_mastery': 'lol/champion-mastery/v4/champion-masteries/by-puuid/{puuid}/by-champion/{championId}', # get a champion mastery by puuid and championId
         'top_n_champion_masteries': 'lol/champion-mastery/v4/champion-masteries/by-puuid/{puuid}/top?count={limit}',  #  get specified number of top champion mastery entries sorted by number of champion points descending
+        'mastery_score': 'lol/champion-mastery/v4/scores/by-puuid/{puuid}',  # get a player's total champion mastery score, which is the sum of individual champion mastery levels
+    }
+
+    _CHAMPION_V3 = {}
+
+    _CLASH_V1 = {}
+
+    _LEAGUE_EXP_V4 = {}
+
+    _LEAGUE_V4 = {}
+
+    _LOL_CHALLENGES_V1 = {}
+
+    _LOL_STATUS_V4 = {}
+
+    _LEAGUE_V4 = {}
+
+    _LEAGUE_EXP_V4 = {}
+
+    _MATCH_V5 = {}
+
+    _SPECTATOR_V4 = {}
+
+    _DATA_DRAGON = {
         # 'data_dragon': 'https://ddragon.leagueoflegends.com/cdn/dragontail-13.11.1.tgz'
+    }
+
+    ENDPOINTS = {
+        'account': 'riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}',
+        **_CHAMPION_MASTERY_V4
     }
