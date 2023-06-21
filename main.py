8817f3c4421b0e7b0ce26c2103244d2ea03d2f0c
timothyckl <timothy.ckl@outlook.com>
2023-06-21 23:26:58 +0800

    added test functions, will eventually make unit tests lol

--- main.py
+++ main.py
@@ -10,9 +10,19 @@ if __name__ == '__main__':
     api_key = os.getenv('RIOT_API_KEY')
     client = Client(api_key, continent='ASIA', region='SG2')
 
-    puuid = client.get_puuid('thermostat', 'tom')
-    # champion_masteries = client.get_champion_mastery(puuid)
-    # champion_mastery = client.get_champion_mastery(puuid, championId=80)
+    # summoner = client.get_summoner('timtamtom')
 
+    # champion_masteries = client.get_champion_mastery(summoner['puuid'])
+    # champion_mastery = client.get_champion_mastery(summoner['puuid'], championId=80)
+    # top_2_champions = client.get_champion_mastery(summoner['puuid'], limit=2)
+    # mastery_score = client.get_mastery_score(summoner['puuid'])
+    # champion_rotation = client.get_champion_rotation()
+    
+    # print(json.dumps(summoner, indent=4))
     # print(json.dumps(champion_masteries, indent=4))
     # print(json.dumps(champion_mastery, indent=4))
+    # print(json.dumps(top_2_champions, indent=4))
+    # print(json.dumps(mastery_score, indent=4))
+    # print(json.dumps(champion_rotation, indent=4))
+    pantheon = client.get_champion('80')
+    print(pantheon.desc)
