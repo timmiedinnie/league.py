572873c10a1c3e29e2acf7cd62170eb4360e4810
timothyckl <timothy.ckl@outlook.com>
2023-06-22 10:44:32 +0800

    test apis

--- main.py
+++ main.py
@@ -17,12 +17,14 @@ if __name__ == '__main__':
     # top_2_champions = client.get_champion_mastery(summoner['puuid'], limit=2)
     # mastery_score = client.get_mastery_score(summoner['puuid'])
     # champion_rotation = client.get_champion_rotation()
-    
+    # pantheon = client.get_champion('80')
+    # boots = client.cache.get_item('1001')
+
     # print(json.dumps(summoner, indent=4))
     # print(json.dumps(champion_masteries, indent=4))
     # print(json.dumps(champion_mastery, indent=4))
     # print(json.dumps(top_2_champions, indent=4))
     # print(json.dumps(mastery_score, indent=4))
     # print(json.dumps(champion_rotation, indent=4))
-    pantheon = client.get_champion('80')
-    print(pantheon.desc)
+    # print(pantheon)
+    # print(boots)
