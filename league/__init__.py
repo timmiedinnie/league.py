4fc928db0ae348367a59ba711d0f6d6a203e14dd
timothyckl <timothy.ckl@outlook.com>
2023-06-22 15:30:59 +0800

    add package info

--- league/__init__.py
+++ league/__init__.py
@@ -1,3 +1,7 @@
 from .client import Client
 
+__title__ = "league"
+__author__ = "timothyckl"
+__doc__ = "Unofficial Python interface for the League of Legends API!"
 __all__ = ['Client']
+__version__ = "0.0.1"
