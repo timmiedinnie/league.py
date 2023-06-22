a9c16b99c1767129556bb88eae8a83e1d41bac98
timothyckl <timothy.ckl@outlook.com>
2023-06-22 15:05:53 +0800

    add mastery unit test

--- tests/test_client.py
+++ tests/test_client.py
@@ -34,3 +34,20 @@ def test_get_summoner(client):
     assert summoner['id'] == 'NgcrA4udzPlTnWKWQmxcNYlZhFMfsUZSGMIIagDGCCCTYaN80t0HGx2XZw'
     assert summoner['name'] == 'timtamtom'
     assert summoner['puuid'] == 'N3UBD62V_MMh46MfiRo3TaMgD2xh5vprhF12IfAsAXzkUpBR-uL02rxgAsrDmOwzwbvBLWYadAeFQg'
+
+# @pytest.mark.parametrize('summoner', ['timtamtom'],)
+
+# def test_get_champion_mastery(client, summoner):
+#     summoner = client.get_summoner('timtamtom')
+#     champion_masteries = client.get_champion_mastery(summoner['puuid'])
+#     print(champion_masteries)
+    # assert champion_masteries[0]['championId'] == 80
+    # assert champion_masteries[0]['championLevel'] == 7
+    # assert champion_masteries[0]['championPoints'] == 100000
+    # assert champion_masteries[0]['lastPlayTime'] == 1616804608000
+    # assert champion_masteries[0]['championPointsSinceLastLevel'] == 80000
+    # assert champion_masteries[0]['championPointsUntilNextLevel'] == 0
+    # assert champion_masteries[0]['chestGranted'] == True
+    # assert champion_masteries[0]['tokensEarned'] == 0
+    # assert champion_masteries[0]['summonerId'] == 'NgcrA4udzPlTnWKWQmxcNYlZhFMfsUZSGMIIagDGCCCTYaN80t0HGx2XZw'
+    # pass
