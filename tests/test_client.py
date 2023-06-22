98d0d39d1ae00aa6533644743e07e4d3d6b0ed42
timothyckl <timothy.ckl@outlook.com>
2023-06-22 14:40:28 +0800

    update client init unit test

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,12 +2,28 @@
 import pytest
 
 @pytest.mark.parametrize('continent, region', [
-    ('ASIA', 'SG2'),
     ('AMERICAS', 'NA1'),
+    ('AMERICAS', 'BR1'),
+    ('AMERICAS', 'LA1'),
+    ('AMERICAS', 'LA2'),
+    ('ASIA', 'KR'),
+    ('ASIA', 'JP1'),
+    ('ASIA', 'OC1'),
+    ('ASIA', 'PH2'),
+    ('ASIA', 'SG2'),
+    ('ASIA', 'TH2'),
+    ('ASIA', 'VN2'),
+    ('EUROPE', 'EUN1'),
     ('EUROPE', 'EUW1'),
+    ('EUROPE', 'RU'),
+    ('EUROPE', 'TR1'),
+    ('EUROPE', 'TW2')
 ])
 
 def test_client_init(api_key, client, continent, region):
+    client.continent = continent
+    client.region = region
+
     assert client.api_key == api_key
     assert client.continent == continent
     assert client.region == region
